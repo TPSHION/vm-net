48c8c3a2335f36cc53a03de930fc89551a2260bf
TPSHION <tpshion@163.com>
2025-04-04 12:20:21 +0800

    Initial Commit

---
+++ vm-net/vm_netApp.swift
@@ -0,0 +1,17 @@
+//
+//  vm_netApp.swift
+//  vm-net
+//
+//  Created by chen on 2025/4/4.
+//
+
+import SwiftUI
+
+@main
+struct vm_netApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
